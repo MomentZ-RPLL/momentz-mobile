1cf97e188dfc97144704ed6e5928e36f43c26280
KaisarV <kaisar.valentino123@gmail.com>
2023-10-20 09:20:25 +0700

    initial commit

---
+++ app/src/main/java/com/kai/momentz/view/landing/LandingActivity.kt
@@ -0,0 +1,12 @@
+package com.kai.momentz.view.landing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kai.momentz.R
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
---
+++ app/src/main/java/com/kai/momentz/view/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.kai.momentz.view.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kai.momentz.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
