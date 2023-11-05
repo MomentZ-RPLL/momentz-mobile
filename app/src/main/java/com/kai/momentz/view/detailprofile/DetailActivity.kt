2859fed6121559c9fcced9d652c03d1bbd4f18eb
JefanyaB <jefayanyalangenya@gmail.com>
2023-11-05 22:01:30 +0700

    push home

---
+++ app/src/main/java/com/kai/momentz/view/detailprofile/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.kai.momentz.view.detailprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kai.momentz.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
